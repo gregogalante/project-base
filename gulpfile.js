cb9389b7c2217decc0682f658f87efb7ca76f8a1
Gregorio Galante <gregogalante@gmail.com>
2016-04-15 10:09:58 +0200

    ntc

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@ var gulp = require('gulp')
 
 // Dependencies
 var sass = require('gulp-sass')
+var concat = require('gulp-concat')
 
 // Deafult task
 gulp.task('default', function () {
   console.log('Welcome to project base')
   // Watch sass files ** gulp-sass
   gulp.watch('./css/**/*.scss', ['sass'])
+  // Watch js files ** gulp-concat
+  gulp.watch('./js/**/*.js', ['concat'])
 })
 
 // Sass tasks
@@ -15,4 +18,11 @@ gulp.task('sass', function () {
   return gulp.src('./css/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'))
+})
+
+// Concat tasks
+gulp.task('concat', function () {
+  return gulp.src(['./js/vendor/*.js', './js/plugins.js', './js/custom.js'])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./js'))
 })
