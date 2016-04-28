16b85015d9205914f397e9ce29514f7c19c905e0
Gregorio Galante <gregogalante@gmail.com>
2016-04-28 04:28:20 +0200

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,10 @@ var gulp = require('gulp')
 // Dependencies gulp plugin css
 var css_sass = require('gulp-sass')
 var css_autoprefixer = require('gulp-autoprefixer')
-var css_globbing = require('gulp-css-globbing')
 
 // Dependencies gulp plugin js
 var js_concat = require('gulp-concat')
 // var babel = require('gulp-babel')
-
-// Dependencies gulp plugin
-// var insert = require('gulp-insert')
 
 // Other dependencies
 var sassdoc = require('sassdoc')
@@ -20,24 +16,14 @@ var run_sequence = require('run-sequence')
 gulp.task('default', function () {
   console.log('Welcome to project base')
   // Watch sass files **
-  gulp.watch('./css/vendor/**/*', ['css'])
-  gulp.watch('./css/base/**/*', ['css'])
-  gulp.watch('./css/modules/**/*', ['css'])
-  gulp.watch('./css/pages/**/*', ['css'])
-  gulp.watch('./css/custom.scss', ['css'])
+  gulp.watch('./css/**/*', ['css'])
   // Watch js files **
-  gulp.watch('./js/**/*', ['js_concat'])
+  gulp.watch('./js/**/*', ['js'])
 })
 
 // CSS tasks
 gulp.task('css', function () {
-  run_sequence('css_globbing', 'css_sass', 'css_autoprefixer')
-})
-
-gulp.task('css_globbing', function () {
-  return gulp.src('./css/vendor/vendor.scss')
-    .pipe(css_globbing())
-    .pipe(gulp.dest('./css'))
+  run_sequence('css_sass', 'css_autoprefixer')
 })
 
 gulp.task('css_autoprefixer', function () {
@@ -53,6 +39,10 @@ gulp.task('css_sass', function () {
 })
 
 // JS tasks
+gulp.task('js', function () {
+  run_sequence('js_concat')
+})
+
 gulp.task('js_concat', function () {
   return gulp.src(['./js/vendor/*.js', './js/plugins.js', './js/custom.js'])
     .pipe(js_concat('main.js'))
