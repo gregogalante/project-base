062136aa8d6229ff903b4891fa228b31590fa968
Gregorio Galante <gregogalante@gmail.com>
2017-05-20 14:25:38 +0200

    ntc

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,25 @@
-// Project-base by G.Galante
-
-// @@ GULPFILE Dependencies
-// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// Dependencies:
 
 var gulp = require('gulp')
 
 // Dependencies gulp plugin css
-var css_sass = require('gulp-sass')
-var css_autoprefixer = require('gulp-autoprefixer')
+var cssSass = require('gulp-sass')
+var cssAutoprefixer = require('gulp-autoprefixer')
 
 // Dependencies gulp plugin js
-var js_concat = require('gulp-concat')
-var js_babel = require('gulp-babel')
-var js_uglify = require('gulp-uglify')
+var jsConcat = require('gulp-concat')
+var jsBabel = require('gulp-babel')
+var jsUglify = require('gulp-uglify')
 
 // Dependencies gulp plugin images
-var img_imagemin = require('gulp-imagemin')
-var img_cache = require('gulp-cache')
+var imgImagemin = require('gulp-imagemin')
+var imgCache = require('gulp-cache')
 
 // Others
 var browserSync = require('browser-sync').create()
 var merge = require('merge-stream')
 
-// @@ GULPFILE Tasks
-// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// Tasks:
 
 // Deafult task
 gulp.task('default', function () {
@@ -40,61 +36,53 @@ gulp.task('default', function () {
   gulp.watch('./src/**/*').on('change', browserSync.reload)
 })
 
-// @@ GULPFILE CSS-Tasks
-// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
 // CSS tasks
 gulp.task('css', function () {
   return gulp.src('./src/css/main.scss')
-  .pipe(css_sass().on('error', css_sass.logError))
+  .pipe(cssSass().on('error', cssSass.logError))
   .pipe(gulp.dest('./src/css'))
 })
-
-// @@ GULPFILE JS-Tasks
-// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 // JS tasks
 gulp.task('js', function () {
   return gulp.src(['./src/js/vendor/*.js', './src/js/custom.js'])
-  .pipe(js_concat('main.js'))
-  .pipe(js_babel({
+  .pipe(jsConcat('main.js'))
+  .pipe(jsBabel({
     presets: ['es2015']
   }))
   .pipe(gulp.dest('./src/js'))
 })
 
-// @@ GULPFILE Build-Tasks
-// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
+// Build task
 gulp.task('build', ['build_css', 'build_js', 'build_img', 'build_copy'])
 
-// CSS tasks
+// Build CSS tasks
 gulp.task('build_css', function () {
   return gulp.src('./src/css/main.scss')
-  .pipe(css_sass({outputStyle: 'compressed'}))
-  .pipe(css_autoprefixer())
+  .pipe(cssSass({outputStyle: 'compressed'}))
+  .pipe(cssAutoprefixer())
   .pipe(gulp.dest('./build/css'))
 })
 
-// JS tasks
+// Build JS tasks
 gulp.task('build_js', function () {
   return gulp.src(['./src/js/vendor/*.js', './src/js/custom.js'])
-  .pipe(js_concat('main.js'))
-  .pipe(js_babel({
+  .pipe(jsConcat('main.js'))
+  .pipe(jsBabel({
     presets: ['es2015']
   }))
-  .pipe(js_uglify())
+  .pipe(jsUglify())
   .pipe(gulp.dest('./build/js'))
 })
 
-// IMG tasks
+// Build IMG tasks
 gulp.task('build_img', function () {
   return gulp.src('src/images/**/*')
-    .pipe(img_cache(img_imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    .pipe(imgCache(imgImagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
     .pipe(gulp.dest('build/images'))
 })
 
-// Others files
+// Copy to build path task
 gulp.task('build_copy', function () {
   var files = gulp.src('./src/*')
     .pipe(gulp.dest('./build'))
