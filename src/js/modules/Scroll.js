1a1b5e0ee284da571209640065f512010b7ad57d
Gregorio Galante <gregogalante@gmail.com>
2018-12-05 21:50:33 +0100

    Update.

--- src/js/modules/Scroll.js
+++ src/js/modules/Scroll.js
@@ -1,7 +1,12 @@
 const Scroll = (() => {
 
   const init = () => {
-    const scroll = new SmoothScroll('a[href*="#"]')
+    const scroll = new SmoothScroll('a[href*="#"]', {
+      offset: () => {
+        const viewportWidth = window.innerWidth || document.documentElement.clientWidth
+        return viewportWidth >= 980 ? 0 : 50
+      }
+    })
   }
 
   return {
